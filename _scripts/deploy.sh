8b316708f0ef6dfc999650646acd5c3cfc2152f1
Jonas Wegener <jonas@steps.fm>
2016-09-30 19:20:55 +0200

    updated deploy.sh

--- _scripts/deploy.sh
+++ _scripts/deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+set -x
 if [ $TRAVIS_BRANCH == 'master' ] ; then
-	echo "Deploying to remote"
-	cd _site
-	git add .
-	git commit -m "Deploy build #$TRAVIS_BUILD_NUMBER"
-	git push --force deploy master
+    # Initialize a new git repo in _site, and push it to our server.
+    cd _site
+    git init
+
+    git remote add deploy "root@104.236.116.254:/www/html"
+    git config user.name "Travis CI"
+    git config user.email "jonas@steps.fm"
+
+    git add .
+    git commit -m "Deploy"
+    git push --force deploy master
 else
-	echo "Not deploying, since this branch isn't master."
+    echo "Not deploying, since this branch isn't master."
 fi
