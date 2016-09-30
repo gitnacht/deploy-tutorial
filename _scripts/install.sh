4c3c3b728fb69846451bbf7af09f1b1c96c0edec
Jonas Wegener <jonas@steps.fm>
2016-09-30 19:00:44 +0200

    added new digital ocean server

--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,8 +2,8 @@
 set -x # Show the output of the following commands (useful for debugging)
 
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_f63a7b37db9b_key -iv $encrypted_f63a7b37db9b_iv -in id_rsa.enc -out id_rsa -d
-rm id_rsa.enc # Don't need it anymore
-chmod 600 id_rsa
-mv id_rsa ~/.ssh/id_rsa
+openssl aes-256-cbc -K $encrypted_f63a7b37db9b_key -iv $encrypted_f63a7b37db9b_iv -in tutorial.enc -out tutorial -d
+rm tutorial.enc # Don't need it anymore
+chmod 600 tutorial
+mv tutorial ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa
