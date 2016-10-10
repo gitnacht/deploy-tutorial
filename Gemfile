117788e2d3c2d6cf977d4d4ed14c6d3e724da054
Jonas Wegener <jonas@steps.fm>
2016-10-10 20:38:00 +0200

    updated travis.yml

--- Gemfile
+++ Gemfile
@@ -20,7 +20,3 @@ gem 'iki'
 # gem "github-pages", group: :jekyll_plugins
 
 # If you have any plugins, put them here!
-group :jekyll_plugins do
-  gem "jekyll-paginate", "~> 1.0"
-  gem 'jekyll-paginate-category'
-end
--- _scripts/install.sh
+++ _scripts/install.sh
@@ -2,7 +2,7 @@
 set -x # Show the output of the following commands (useful for debugging)
 
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_5b76d4a8d6f2_key -iv $encrypted_5b76d4a8d6f2_iv -in chathappens_deploy.enc -out chathappens_deploy -d
+openssl aes-256-cbc -K $encrypted_0b522ad0db2b_key -iv $encrypted_0b522ad0db2b_iv -in chathappens_deploy.enc -out chathappens_deploy -d
 rm chathappens_deploy.enc # Don't need it anymore
 chmod 600 chathappens_deploy
 mv chathappens_deploy ~/.ssh/id_rsa
